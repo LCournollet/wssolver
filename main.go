de9c2a65b15bf1eee746b3ecf189e8fe80a8e917
Louis <l.cournollet@gmail.com>
2022-07-27 00:27:24 +0200

    Patch final

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 
 	"github.com/01-edu/z01"
@@ -21,6 +20,19 @@ var field = [10][10]rune{
 	{'n', 'n', 'c', 'n', 'c', 'w', 'b', 'o', 'b', 'n'},
 	{'f', 'o', 'u', 'r', 'c', 'h', 'e', 't', 't', 'e'},
 	{'b', 'a', 'i', 'e', 's', 't', 'h', 'n', 'w', 's'},
+}
+
+var Testfield = [10][10]rune{
+	{'c', 'o', 't', 'd', 't', 'r', 's', 'n', 'e', 'c'},
+	{'z', 'e', 'e', 'o', 't', 'e', 'o', 'h', 'u', 'c'},
+	{'e', 'u', 'h', 'h', 't', 'r', 'l', 'a', 'o', 'z'},
+	{'p', 'z', 'w', 'z', 'e', 'r', 'e', 'a', 'a', 'f'},
+	{'e', 's', 't', 'p', 'r', 't', 'a', 'e', 'r', 'e'},
+	{'s', 'a', 'z', 'y', 'u', 'i', 'o', 'e', 's', 'd'},
+	{'p', 'a', 'e', 'z', 'i', 't', 'c', 'd', 't', 'e'},
+	{'n', 'n', 'c', 'n', 'c', 'w', 'b', 'o', 'b', 'n'},
+	{'f', 'o', 'u', 'r', 'z', 'h', 'e', 't', 't', 'e'},
+	{'b', 'a', 'z', 'e', 's', 't', 'h', 'n', 'w', 's'},
 }
 
 //======================================================================================================================
@@ -52,6 +64,42 @@ func PrintField(field [10][10]rune) {
 func LOT_OF_SPACE_SHEEESH() {
 	for i := 0; i < 3; i++ {
 		z01.PrintRune('\n')
+	}
+}
+
+func PrintAllWords(Words []string) {
+	z01.PrintRune('[')
+	for i := 0; i < len(Words); i++ {
+		for j := 0; j < len(Words[i]); j++ {
+			z01.PrintRune(rune(Words[i][j]))
+		}
+		if i != len(Words)-1 {
+			z01.PrintRune(' ')
+			z01.PrintRune(',')
+			z01.PrintRune(' ')
+		}
+	}
+	z01.PrintRune(']')
+	z01.PrintRune('\n')
+}
+
+func PrintNoWords(Bool bool) {
+	if Bool == false {
+		Array := [1]string{"Words Founded: "}
+		for i := 0; i < len(Array); i++ {
+			for j := 0; j < len(Array[i]); j++ {
+				z01.PrintRune(rune(Array[i][j]))
+			}
+		}
+	}
+	if Bool == true {
+		Array := [1]string{"No words found"}
+		for i := 0; i < len(Array); i++ {
+			for j := 0; j < len(Array[i]); j++ {
+				z01.PrintRune(rune(Array[i][j]))
+			}
+			z01.PrintRune('\n')
+		}
 	}
 }
 
@@ -89,6 +137,8 @@ func NewArray(FirstLetter rune, SecondLetter rune, Words []string) []string {
 }
 
 func WordScrabbleSolver(field [10][10]rune) {
+	FinalArray := []string{}
+	Temp := []string{}
 	words := GetArrayFromWords()
 
 	for i := 0; i < len(field); i++ {
@@ -98,24 +148,30 @@ func WordScrabbleSolver(field [10][10]rune) {
 				if field[i][j] == rune(words[WordIncrease][0]) {
 
 					if j != len(field[i])-1 && field[i][j+1] == rune(words[WordIncrease][1]) {
-						// NewWordsList := NewArray(field[i][j], field[i][j+1], words)
-						VeryfHorizontal(i, j, words[WordIncrease], field)
+						Temp = VeryfHorizontal(i, j, words[WordIncrease], field)
+						FinalArray = append(FinalArray, Temp...)
 
 					} else if i != len(field)-1 && field[i+1][j] == rune(words[WordIncrease][1]) {
-						// NewWordsList := NewArray(field[i][j], field[i][j-1], words)
-						VeryfVertical(i, j, words[WordIncrease], field)
-
+						Temp = VeryfVertical(i, j, words[WordIncrease], field)
+						FinalArray = append(FinalArray, Temp...)
 					} else if i != len(field)-1 && j != len(field[i])-1 && field[i+1][j+1] == rune(words[WordIncrease][1]) {
-						// NewWordsList := NewArray(field[i][j], field[i][j-1], words)
-						VeryfBottomRight(i, j, words[WordIncrease], field)
+						Temp = VeryfBottomRight(i, j, words[WordIncrease], field)
+						FinalArray = append(FinalArray, Temp...)
 					} else if i != 0 && j != len(field[i])-1 && field[i-1][j+1] == rune(words[WordIncrease][1]) {
-						// NewWordsList := NewArray(field[i][j], field[i][j-1], words)
-						VeryfTopRight(i, j, words[WordIncrease], field)
+						Temp = VeryfTopRight(i, j, words[WordIncrease], field)
+						FinalArray = append(FinalArray, Temp...)
 
 					}
 				}
 			}
 		}
+	}
+
+	if len(FinalArray) > 1 {
+		PrintNoWords(false)
+		PrintAllWords(FinalArray)
+	} else {
+		PrintNoWords(true)
 	}
 }
 
@@ -123,17 +179,21 @@ func VeryfHorizontal(x int, y int, NewWordsList string, field [10][10]rune) []st
 	array := []string{}
 	Count := 0
 	LetterIncrease := 0
+
 	for j := y; j < len(field[x]); j++ {
+
 		if field[x][j] == rune(NewWordsList[LetterIncrease]) {
 			Count += 1
+
 			if Count == len(NewWordsList) {
 				array = append(array, NewWordsList)
-				fmt.Println("Found: ", NewWordsList)
+				// fmt.Println("Found: ", NewWordsList)
 				break
 			}
 			if LetterIncrease != len(NewWordsList)-1 {
 				LetterIncrease++
 			}
+
 		} else {
 			Count = 0
 		}
@@ -147,16 +207,19 @@ func VeryfVertical(x int, y int, NewWordsList string, field [10][10]rune) []stri
 	LetterIncrease := 0
 
 	for i := x; i < len(field); i++ {
+
 		if field[i][y] == rune(NewWordsList[LetterIncrease]) {
 			Count += 1
+
 			if Count == len(NewWordsList) {
 				array = append(array, NewWordsList)
-				fmt.Println("Found: ", NewWordsList)
+				// fmt.Println("Found: ", NewWordsList)
 				break
 			}
 			if LetterIncrease != len(NewWordsList)-1 {
 				LetterIncrease++
 			}
+
 		} else {
 			Count = 0
 		}
@@ -170,22 +233,28 @@ func VeryfBottomRight(x int, y int, NewWordsList string, field [10][10]rune) []s
 	LetterIncrease := 0
 	i := x
 	j := y
+
 	for ; i < len(field); i++ {
+
 		if field[i][j] == rune(NewWordsList[LetterIncrease]) {
 			Count++
+
 			if Count == len(NewWordsList) {
 				array = append(array, NewWordsList)
-				fmt.Println("Found: ", NewWordsList)
+				// fmt.Println("Found: ", NewWordsList)
 				break
 			}
+
 			if LetterIncrease != len(NewWordsList)-1 {
 				LetterIncrease++
 			}
+
 			if j != len(field[i])-1 {
 				j++
 			} else {
 				break
 			}
+
 		} else {
 			Count = 0
 		}
@@ -199,22 +268,28 @@ func VeryfTopRight(x int, y int, NewWordsList string, field [10][10]rune) []stri
 	LetterIncrease := 0
 	i := x
 	j := y
+
 	for ; j < len(field); j++ {
+
 		if field[i][j] == rune(NewWordsList[LetterIncrease]) {
 			Count++
+
 			if Count == len(NewWordsList) {
 				array = append(array, NewWordsList)
-				fmt.Println("Found: ", NewWordsList)
+				// fmt.Println("Found: ", NewWordsList)
 				break
 			}
+
 			if LetterIncrease != len(NewWordsList)-1 {
 				LetterIncrease++
 			}
+
 			if i != 0 {
 				i--
 			} else {
 				break
 			}
+
 		} else {
 			Count = 0
 		}
@@ -226,15 +301,7 @@ func VeryfTopRight(x int, y int, NewWordsList string, field [10][10]rune) []stri
 //====================================================[↓ MAIN ↓]=======================================================
 //======================================================================================================================
 func main() {
-	// y := 0
-	// for j := y; j < 5; j++ {
-	// 	fmt.Println(j)
-	// 	// k := y
-	// 	fmt.Println("Hello")
-	// 	if j == 4 {
-	// 		j = 0
-	// 	}
-	// }
-	PrintField(field)
-	WordScrabbleSolver(field)
+	FIELD := field
+	PrintField(FIELD)
+	WordScrabbleSolver(FIELD)
 }
